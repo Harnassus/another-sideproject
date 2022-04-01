3b64306cb5bd9d0a7c1abf8aa47a4c110aadee50
Michael Festus <micfestus45@gmail.com>
2022-04-01 15:11:51 +0100

    Working with API's

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import './style.css';
 
-function App() {
+function App () {
+  const url = 'https://www.boredapi.com/api/activity';
+
+  const [Activity, setActivity] = useState ([{}]);
+  const [loading, setloading] = useState (false);
+
+  useEffect(() => {
+    fetch(`${url}`).then((response) => response.json()
+    ).then(data => {
+      setActivity(data)
+
+      const timeout = setTimeout(() => {
+        setloading(true)
+      }, 500)
+    })
+    
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Are you bored?</h1>
+      <div>
+        {loading ?  Activity.activity  : (
+          <div className="spinner">
+            <div></div>
+            <div></div>
+          </div>
+        )}
+         
+      </div>
     </div>
   );
 }
